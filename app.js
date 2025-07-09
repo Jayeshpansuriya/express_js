694d5628ffe015cca8f759e30bc48bbfc3792e48
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-09 12:31:35 +0530

    query parameters in url

--- app.js
+++ app.js
@@ -22,12 +22,24 @@ console.log(import.meta.filename);
 const staticpath = path.join(import.meta.dirname,"public");
 app.use("/public",express.static(staticpath));
 
+app.get("/product",(req,res)=>{
+  console.log(req.query);
+  const formattedQuery = req.query.page ? req.query.page.replace(/-/g," ") : '';
+  const formattedQuery1 = req.query.limit ? req.query.limit.replace(/-/g," ") : '';
+  res.send(`<h1>Welcome to product page ${formattedQuery} ${formattedQuery1} page</h1> `)
+
+})
+
+
+
+
 app.get("/profile/:username",(req,res)=>{
   console.log(req.params);
   // res.send("hii");
 res.send(`<h1>Welcome to ${req.params.username} profile</h1>`);
 
 });
+
 app.get("/profile/:username/article/:slug",(req,res)=>{
   console.log(req.params);
   const formatedslug = req.params.slug.replace(/-/g," ");
