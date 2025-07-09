b5e171ec03a2e74f8b234b580f48dff2cca98e1c
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-09 14:33:32 +0530

    404 and geting data from form in to console

--- app.js
+++ app.js
@@ -5,9 +5,29 @@ import { fileURLToPath } from 'url';  // ✅ Required
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
 
 
+app.get("/",(req,res)=>{
+  res.send("<h1>Welcome to home page</h1>");
+  res.sendFile(path.join(import.meta.dirname,"public","index.html"));  
+ });
+
+
+// app.get("/contact",(req,res)=>{
+//   console.log(req.query);
+//   // res.send("<h1>Welcome to contact page</h1>");
+//   res.send("oK");
+
+// })  
+
+app.post("/contact",(req,res)=>{
+  console.log(req.body);
+  res.send("<h1>Welcome to contact page</h1>");
+res.redirect("/"); // Redirects to the home page
+
+})  
 
 // const response =  await fetch("https://jsonplaceholder.typicode.com/posts");
 // const json = await response.json();
@@ -16,36 +36,42 @@ app.use(express.static('public'));
 
 
 
-console.log(import.meta.dirname);
-console.log(import.meta.filename);
+// console.log(import.meta.dirname);
+// console.log(import.meta.filename);
 
-const staticpath = path.join(import.meta.dirname,"public");
-app.use("/public",express.static(staticpath));
+// const staticpath = path.join(import.meta.dirname,"public");
+// app.use("/public",express.static(staticpath));
 
-app.get("/product",(req,res)=>{
-  console.log(req.query);
-  const formattedQuery = req.query.page ? req.query.page.replace(/-/g," ") : '';
-  const formattedQuery1 = req.query.limit ? req.query.limit.replace(/-/g," ") : '';
-  res.send(`<h1>Welcome to product page ${formattedQuery} ${formattedQuery1} page</h1> `)
+// app.get("/product",(req,res)=>{
+//   console.log(req.query);
+//   const formattedQuery = req.query.page ? req.query.page.replace(/-/g," ") : '';
+//   const formattedQuery1 = req.query.limit ? req.query.limit.replace(/-/g," ") : '';
+//   res.send(`<h1>Welcome to product page ${formattedQuery} ${formattedQuery1} page</h1> `)
 
-})
-
-
+// })
 
 
-app.get("/profile/:username",(req,res)=>{
-  console.log(req.params);
-  // res.send("hii");
-res.send(`<h1>Welcome to ${req.params.username} profile</h1>`);
 
+
+// app.get("/profile/:username",(req,res)=>{
+//   console.log(req.params);
+//   // res.send("hii");
+// res.send(`<h1>Welcome to ${req.params.username} profile</h1>`);
+
+// });
+
+// app.get("/profile/:username/article/:slug",(req,res)=>{
+//   console.log(req.params);
+//   const formatedslug = req.params.slug.replace(/-/g," ");
+// res.send(`<h1>Welcome to ${req.params.username} article ${formatedslug}</h1>`);
+
+// });
+
+
+app.use((req,res)=>{
+ return res.status(404).sendFile(path.join(import.meta.dirname,"views","404.html"));
 });
 
-app.get("/profile/:username/article/:slug",(req,res)=>{
-  console.log(req.params);
-  const formatedslug = req.params.slug.replace(/-/g," ");
-res.send(`<h1>Welcome to ${req.params.username} article ${formatedslug}</h1>`);
-
-});
 
 
 const PORT = 3008;
